5fde022e91163dd25ccd501640e287f69d338f0c
OmarCardoze <cardozeomar@gmail.com>
2021-01-16 12:19:15 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let colorPicker = document.getElementById('colorPicker')
+
+colorPicker.addEventListener('input', e => {
+    let background = document.getElementById('background')
+    background.style.background = e.target.value
+})
